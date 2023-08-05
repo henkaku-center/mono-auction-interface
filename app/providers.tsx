3411ea9e91df969538b64eab4daff1cd5fe43e53
yuki-o1o5 <jkksst1712@gmail.com>
2023-08-05 15:09:36 -0700

    style: set provider

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import theme from "@/constants/theme";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
