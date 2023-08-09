7f27f581442c6ac44503bdacc00b075a46507884
yuki-o1o5 <jkksst1712@gmail.com>
2023-08-08 18:13:19 -0700

    docs: move theme.ts to constants dir

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "dark",
+  initialColorMode: "light",
   useSystemColorMode: false,
 };
 
