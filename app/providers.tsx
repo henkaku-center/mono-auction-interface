e67b81aafb547e55aed5c3f6ad458729f4557ec6
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-08-16 11:20:35 +0900

    feat: added wagmi for react

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,11 +3,27 @@
 import theme from "@/constants/theme";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { polygon, polygonMumbai } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [polygon, polygonMumbai],
+  [publicProvider()],
+)
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+})
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </CacheProvider>
+    <WagmiConfig config={config}>
+      <CacheProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </CacheProvider>
+    </WagmiConfig>
   );
 }
