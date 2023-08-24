ea387055bb905a2aac0f5dd29d60493392d58716
CanIyu <35588557+CanIyu@users.noreply.github.com>
2023-08-25 00:43:08 +0900

    fixed some button issues

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["dummyimage.com"],
   },
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
