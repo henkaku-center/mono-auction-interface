82911e1e46383506f8fb1e06dd05bb6ac0583f95
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-09-06 17:27:47 +0900

    tmp

--- components/SellForm/index.tsx
+++ components/SellForm/index.tsx
@@ -2,60 +2,70 @@
 
 import 'react-datepicker/dist/react-datepicker.css'
 import { FC, useState } from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import {
+  useForm,
+  SubmitHandler,
+  Controller,
+  useFieldArray,
+} from 'react-hook-form'
 import {
   Box,
   Button,
   Center,
+  Container,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
   Grid,
   GridItem,
   Heading,
+  Input,
+  Textarea,
   VStack,
 } from '@chakra-ui/react'
 import { useUploadImageFile, useUploadMetadataJson } from '@/hooks/usePinata'
-import { SellInput, SellTextarea } from '../Inputs'
-import { FormData } from '@/types'
+import { MonoNFTRegisterFormData } from '@/types'
 
 const SellForm: FC = () => {
   const [imageIPFSHash, setImageIPFSHash] = useState<string>('')
   const {
     control,
-    watch,
-    register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>()
+  } = useForm<MonoNFTRegisterFormData>({
+    defaultValues: {
+      name: '',
+      image: null,
+      description: '',
+      rule: '',
+      donor: '',
+      expiresDuration: 0,
+      sharesOfCommunityToken: [{ shareHolder: '', shareRatio: 0 }],
+      owner: '',
+    },
+  })
+  const {
+    fields: revenueSharingFields,
+    append: revenueSharingAppend,
+    remove: revenueSharingRemove,
+  } = useFieldArray({
+    control,
+    name: 'sharesOfCommunityToken',
+  })
 
-  const watchedStartDate: Date | undefined = watch('startDate')
   const uploadFile = useUploadImageFile()
   // const uploadMetadata = useUploadMetadataJson();
 
-  const onSubmit: SubmitHandler<FormData> = async (data) => {
+  const validateFileSize = (file: File | null, limit: number) => {
+    if (!file) return true
+    return file.size / (1024 * 1024) > limit ? `Upto ${limit}MB` : true
+  }
+
+  const onSubmit: SubmitHandler<MonoNFTRegisterFormData> = async (data) => {
     try {
-      const {
-        productName,
-        image,
-        description,
-        rule,
-        minimumCost,
-        allocationAddress,
-        allocationPercentage,
-        startDate,
-        endDate,
-      } = data
-      console.log(
-        productName,
-        image,
-        description,
-        rule,
-        minimumCost,
-        allocationAddress,
-        allocationPercentage,
-        startDate,
-        endDate
-      )
-      if (image) {
-        const newImageIPFSHash = await uploadFile(image)
+      if (data.image) {
+        const newImageIPFSHash = await uploadFile(data.image)
         setImageIPFSHash(newImageIPFSHash)
         console.log(newImageIPFSHash)
       } else {
@@ -67,176 +77,236 @@ const SellForm: FC = () => {
   }
 
   return (
-    <>
-      <VStack w={['100%', '82%']} spacing="5%" align="start">
-        <Box p={4}>
-          <Heading as="h1" noOfLines={1} mb={5}>
-            商品を出品する
-          </Heading>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <SellInput
-              id="productName"
-              label="商品名"
-              type="text"
-              control={control}
-              rules={{ required: 'This field is required' }}
-              placeholder="商品名"
-              register={register}
-              registerName="productName"
-              error={errors.productName}
-              width={['240px', '360px']}
-            />
+    <Container>
+      <Box p={4}>
+        <Heading as="h1" noOfLines={1} mb={5}>
+          商品を出品する
+        </Heading>
 
-            <Grid templateColumns="repeat(5, 1fr)" mb={5}>
-              <GridItem colSpan={1.5}>
-                <div>商品画像</div>
-              </GridItem>
-              <GridItem colSpan={3}>
-                <Button>ファイルを選択</Button>
-              </GridItem>
-            </Grid>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            control={control}
+            name="name"
+            render={({ field, fieldState: { error } }) => (
+              <FormControl isInvalid={!!error} mb={5} isRequired>
+                <Grid templateColumns="repeat(5, 1fr)">
+                  <GridItem colSpan={1.5}>
+                    <FormLabel>商品名</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={3}>
+                    <Input
+                      onChange={field.onChange}
+                      value={field.value}
+                      mr={2}
+                      placeholder="商品名"
+                    />
+                    {error && (
+                      <FormErrorMessage>{error.message}</FormErrorMessage>
+                    )}
+                  </GridItem>
+                </Grid>
+              </FormControl>
+            )}
+          />
 
-            <SellTextarea
-              id="description"
-              label="説明文"
-              control={control}
-              rules={{ required: 'This field is required' }}
-              placeholder="説明文を入力してください"
-              register={register}
-              registerName="description"
-              error={errors.description}
-              width={['240px', '360px']}
-            />
+          <Controller
+            control={control}
+            name="image"
+            rules={{
+              required: 'REQUIRED_INPUT',
+              validate: (v) => validateFileSize(v, 3),
+            }}
+            render={({ field: { onChange }, fieldState }) => (
+              <FormControl isRequired>
+                <Grid templateColumns="repeat(5, 1fr)" mb={5}>
+                  <GridItem colSpan={1.5}>
+                    <FormLabel htmlFor="imageFile" id="imageFile">
+                      {'商品画像'}
+                    </FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={3}>
+                    <>
+                      <Input
+                        variant="unstyled"
+                        p={1}
+                        id="imageFile"
+                        type="file"
+                        accept={'image/*'}
+                        isRequired={true}
+                        onChange={(e) =>
+                          e.target.files && e.target.files[0].size
+                            ? onChange(e.target.files[0])
+                            : false
+                        }
+                      />
+                      <Box color="red.300">{fieldState.error?.message}</Box>
+                    </>
+                  </GridItem>
+                </Grid>
+              </FormControl>
+            )}
+          />
 
-            <SellTextarea
-              id="rule"
-              label="ルール"
-              control={control}
-              rules={{
-                required: 'This field is required',
-              }}
-              placeholder="説明文を入力してください"
-              register={register}
-              registerName="rule"
-              error={errors.rule}
-              width={['240px', '360px']}
-            />
+          <Controller
+            control={control}
+            name="description"
+            render={({ field, fieldState: { error } }) => (
+              <FormControl isInvalid={!!error} mb={5} isRequired>
+                <Grid templateColumns="repeat(5, 1fr)">
+                  <GridItem colSpan={1.5}>
+                    <FormLabel>説明文</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={3}>
+                    <Textarea
+                      onChange={field.onChange}
+                      value={field.value}
+                      mr={2}
+                      placeholder="説明文"
+                    />
+                    {error && (
+                      <FormErrorMessage>{error.message}</FormErrorMessage>
+                    )}
+                  </GridItem>
+                </Grid>
+              </FormControl>
+            )}
+          />
 
-            <SellInput
-              id="minimumCost"
-              label="最低落札価格"
-              type="number"
-              control={control}
-              rules={{
-                required: 'This field is required',
-                validate: (value: string) => {
-                  const num = parseFloat(value)
-                  if (num < 0) {
-                    return 'The number must be greater than or equal to 0'
-                  }
-                  return true
-                },
-              }}
-              placeholder="300"
-              register={register}
-              registerName="minimumCost"
-              error={errors.minimumCost}
-              width={['120px', '240px']}
-              unit="円"
-            />
+          <Controller
+            control={control}
+            name="description"
+            render={({ field, fieldState: { error } }) => (
+              <FormControl isInvalid={!!error} mb={5} isRequired>
+                <Grid templateColumns="repeat(5, 1fr)">
+                  <GridItem colSpan={1.5}>
+                    <FormLabel>ルール</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={3}>
+                    <Textarea
+                      onChange={field.onChange}
+                      value={field.value}
+                      mr={2}
+                      placeholder="ルール"
+                    />
+                    {error && (
+                      <FormErrorMessage>{error.message}</FormErrorMessage>
+                    )}
+                  </GridItem>
+                </Grid>
+              </FormControl>
+            )}
+          />
 
-            <SellInput
-              id="allocationAddress"
-              label="配分"
-              type="email"
-              control={control}
-              rules={{
-                required: 'This field is required',
-                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              }}
-              placeholder="アドレス"
-              register={register}
-              registerName="allocationAddress"
-              error={errors.allocationAddress}
-              width={['240px', '360px']}
-            />
+          <Controller
+            control={control}
+            name="name"
+            render={({ field, fieldState: { error } }) => (
+              <FormControl isInvalid={!!error} mb={5} isRequired>
+                <Grid templateColumns="repeat(5, 1fr)">
+                  <GridItem colSpan={1.5}>
+                    <FormLabel>寄附者</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={3}>
+                    <Input
+                      onChange={field.onChange}
+                      value={field.value}
+                      mr={2}
+                      placeholder="0xabc1234..."
+                    />
+                    {error && (
+                      <FormErrorMessage>{error.message}</FormErrorMessage>
+                    )}
+                  </GridItem>
+                </Grid>
+              </FormControl>
+            )}
+          />
 
-            <SellInput
-              id="allocationPercentage"
-              type="number"
-              control={control}
-              rules={{
-                required: 'This field is required',
-                validate: (value: string) => {
-                  const num = parseFloat(value)
-                  if (num < 0) {
-                    return 'The number must be greater than or equal to 0'
-                  }
-                  return true
-                },
-              }}
-              placeholder="30"
-              register={register}
-              registerName="allocationPercentage"
-              error={errors.allocationPercentage}
-              width={['120px', '240px']}
-              unit="%"
-            />
+          <Controller
+            control={control}
+            name="expiresDuration"
+            render={({ field, fieldState: { error } }) => (
+              <FormControl isInvalid={!!error} mb={5} isRequired>
+                <Grid templateColumns="repeat(5, 1fr)">
+                  <GridItem colSpan={1.5}>
+                    <FormLabel>使用期限</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={3}>
+                    <Input
+                      type="number"
+                      onChange={field.onChange}
+                      value={field.value}
+                      mr={2}
+                    />
+                    {error && (
+                      <FormErrorMessage>{error.message}</FormErrorMessage>
+                    )}
+                  </GridItem>
+                </Grid>
+              </FormControl>
+            )}
+          />
 
-            <SellInput
-              id="startDate"
-              label="販売開始日"
-              type="date"
-              control={control}
-              rules={{
-                required: 'This field is required',
-                validate: (value: Date) => {
-                  const selectedDate = new Date(value)
-                  const today = new Date()
-                  today.setHours(0, 0, 0, 0)
-                  selectedDate.setHours(0, 0, 0, 0)
-                  return selectedDate >= today || 'The date must be after today'
-                },
-              }}
-              placeholder="開始日"
-              register={register}
-              registerName="startDate"
-              error={errors.startDate}
-            />
+          {revenueSharingFields.map((field, index) => (
+            <Flex justifyContent="flex-end" key={field.id} mb={2}>
+              <Controller
+                control={control}
+                name={`sharesOfCommunityToken.${index}.shareHolder`}
+                rules={{
+                  required: '必須項目です',
+                }}
+                render={({ field: { onChange, value } }) => (
+                  <>
+                    <Input
+                      variant="outline"
+                      type="text"
+                      placeholder="0xabc1234..."
+                      value={value}
+                      isRequired
+                      onChange={(v) => {
+                        onChange(v)
+                      }}
+                    />
+                  </>
+                )}
+              />
+              <Controller
+                control={control}
+                name={`sharesOfCommunityToken.${index}.shareRatio`}
+                rules={{
+                  required: '必須項目です',
+                }}
+                render={({ field: { onChange, value } }) => (
+                  <Box>
+                    <Flex alignItems="center">
+                      <Input
+                        variant="outline"
+                        type="number"
+                        width="60px"
+                        ml={2}
+                        mr={1}
+                        placeholder="0"
+                        textAlign="right"
+                        value={String(value)}
+                        isRequired
+                        onChange={(v) => {
+                          onChange(v)
+                        }}
+                      />
+                      %
+                    </Flex>
+                  </Box>
+                )}
+              />
+            </Flex>
+          ))}
 
-            <SellInput
-              id="endDate"
-              label="販売終了日"
-              type="date"
-              control={control}
-              rules={{
-                required: 'This field is required',
-                validate: (value: Date) => {
-                  if (!watchedStartDate) {
-                    return 'The start date must be set before setting the end date'
-                  }
-                  const endDate = new Date(value)
-                  const startDate = new Date(watchedStartDate)
-                  endDate.setHours(0, 0, 0, 0)
-                  startDate.setHours(0, 0, 0, 0)
-                  return (
-                    endDate >= startDate ||
-                    'The end date must be after the start date'
-                  )
-                },
-              }}
-              placeholder="終了日"
-              register={register}
-              registerName="endDate"
-              error={errors.endDate}
-            />
-            <Center>
-              <Button type="submit">出品する</Button>
-            </Center>
-          </form>
-        </Box>
-      </VStack>
-    </>
+          <Button width="full" type="submit">
+            出品する
+          </Button>
+        </form>
+      </Box>
+    </Container>
   )
 }
 
